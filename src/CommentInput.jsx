1bd8a3d9dcd17b7f6a48af8192725437ab8fa74a
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-12-31 18:23:59 +0100

    Create post detail page, as well as comment input controller with post request handler

---
+++ src/CommentInput.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+
+// eslint-disable-next-line react/prop-types
+const CommentInput = ({ postId }) => {
+  const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const submitComment = async () => {
+    // Assuming you have an API endpoint to handle the comment submission
+    try {
+      const response = await fetch(`http://localhost:3000/${postId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ author, content }),
+      });
+      if (response.ok) {
+        // Handle success, e.g., update UI or trigger a reload of comments
+        console.log('Comment submitted successfully!');
+      } else {
+        // Handle error, e.g., display an error message
+        console.error('Error submitting comment');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents the default form submission and page reload
+
+    if (author.trim() !== '' && content.trim() !== '') {
+      submitComment();
+      setAuthor('');
+      setContent('');
+      window.location.reload();
+    }
+  };
+
+  return (
+    <div>
+      <h2>Add a New Comment</h2>
+      <label>
+        User Name:
+        <input
+          type='text'
+          value={author}
+          onChange={handleAuthorChange}
+          name='author'
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Comment Content:
+        <textarea
+          value={content}
+          onChange={handleContentChange}
+          name='content'
+          required
+        ></textarea>
+      </label>
+      <br />
+      <button onClick={handleSubmit}>Add Comment</button>
+    </div>
+  );
+};
+
+export default CommentInput;
--- src/PostDetails.jsx
+++ src/PostDetails.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CommentInput from './CommentInput';
 
 export default function Post() {
     let {postId} = useParams();
 
     const [post, setPost] = useState(null);
-    const [comments, setComments] = useState(null);
+    const [comments, setComments] = useState([]);
 
 
 
@@ -21,16 +22,38 @@ export default function Post() {
           return response.json();
         })
         .then((response) => {
-            console.log(response)
-            setPost(response)})
+          const {post, comments} = response;
+            setPost(post);
+            setComments(comments);
+          })
         .catch((error) => console.error(error));
     }, [postId]);
 
   return (
     <>
-      <h1>Welcome to Post details</h1>
-      <p>{post.content}</p>
-      {/* <p>{comments.length}</p> */}
+      {post ? (
+        <div className='post'>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <p>{post.date_formatted}</p>
+        </div>
+      ) : (
+        <p>Post is loading... </p>
+      )}
+      <CommentInput postId={postId} />
+      {comments.length === 0 ? (
+        <p>No comments yet</p>
+      ) : (
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment._id}>
+              <p>Author: {comment.author}</p>
+              <p>{comment.content}</p>
+              <p>Date: {comment.date_formatted}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
