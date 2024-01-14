2ca1ad309b484a084920d4b2df98ed77beae9249
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2024-01-14 19:12:44 +0100

    Continue styling, main page is done, need to do post_detail page, and add responsibility for both of them

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,33 +1,46 @@
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-    const [open, setOpen] = useState(false);
-    
+  const [open, setOpen] = useState(false);
 
-      const handleIconClick = () => {
-        setOpen(!open);
-      };
+  const handleIconClick = () => {
+    setOpen(!open);
+  };
 
-    return (
-      <div className='header'>
-        <div className='imageHeader'>
-          <Link to='/'>
-            <img
-              src='https://cdn.pixabay.com/photo/2016/01/09/18/28/notepad-1130743_1280.jpg'
-              alt=''
-            />
-            <p className='pacifico'>Little Boar traveling</p>
-          </Link>
-        </div>
-        <div className='headerButton'>
-          <div id='nav-icon3' onClick={handleIconClick} className={open ? 'open' : ''}>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+  return (
+    <div className='header'>
+      <div className='imageHeader'>
+        <Link to='/'>
+          <img
+            src='https://cdn.pixabay.com/photo/2016/01/09/18/28/notepad-1130743_1280.jpg'
+            alt=''
+          />
+          <p className='pacifico'>Little Boar traveling</p>
+        </Link>
+      </div>
+      <div className='headerButton'>
+        <div
+          id='nav-icon3'
+          onClick={handleIconClick}
+          className={open ? 'open' : ''}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
-    );
+      <nav className={!open ? 'closed' : ''}>
+        <ul className='roboto'>
+          <li>destinations</li>
+          <li>camping & hiking gear</li>
+          <li>van life</li>
+          <li>travel inspiration</li>
+          <li>shop</li>
+          <li>work with me</li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
--- src/Post.jsx
+++ src/Post.jsx
@@ -25,23 +25,55 @@ export default function Post() {
   return (
     <>
       <Header />
+      <h2 className='robotoBold postListHeader'>recent posts</h2>
       {posts ? (
-        <ul>
+        <ul className='postList'>
           {posts.map((post) => (
             <li key={post.id}>
-              <Link to={`/${post._id}`}>
+              <Link className='robotoBold' to={`/${post._id}`}>
                 <h2>{post.title}</h2>
+                <img src={post.image} alt='' />
               </Link>
-              <p>{post.content}</p>
-              <p>{post.date_formatted}</p>
-              <FontAwesomeIcon icon={faComment} />
-              <p>{post.comments.length}</p>
             </li>
           ))}
         </ul>
       ) : (
         <p>Loading posts...</p>
       )}
+      <h2 className='robotoBold postListHeader'>About me</h2>
+      <div className='aboutMe'>
+        <img
+          src='https://cdn.pixabay.com/photo/2023/03/11/22/14/ai-generated-7845433_1280.jpg'
+          alt=''
+        />
+        <p className='robotoParagraph'>
+          Greetings, fellow wanderers of the digital forest! I'm the spirited
+          and adventurous Little Boar, a tiny trotter on a grand journey through
+          the enchanting landscapes of life. With a heart full of curiosity and
+          a snout ready for exploration, I embark on thrilling escapades,
+          discovering hidden gems, and forging friendships with the creatures I
+          encounter. Join me as I traverse through meadows of imagination, dive
+          into rivers of creativity, and climb mountains of possibility. Whether
+          it's chasing dreams or sniffing out new experiences, I'm here to share
+          the joyous tales of my travels. Through this blog, I invite you to
+          wander alongside me, to embrace the magic of the unknown, and to revel
+          in the joy of every tiny discovery. Together, let's celebrate the
+          beauty of the journey and the endless wonders that await just around
+          the bend. So, fasten your imaginary backpacks, fluff up your imaginary
+          map, and let's set forth on this whimsical expedition together!
+          Adventure awaits!
+        </p>
+        <p className='robotoParagraph italic'>Little Boar</p>
+      </div>
+      <div className='bottomBorder postListHeader'></div>
+      <div className='footer'>
+        <div className='links'>
+          <a href=''>Privacy & Disclosure Policy</a>
+          <a href=''>About Little Boar traveling</a>
+          <a href=''>Contact</a>
+        </div>
+        <p>© 2024 Copyright Little Boar traveling</p>
+      </div>
     </>
   );
 }
